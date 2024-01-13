b4a9a0e6442afbf8064cf2abd888e15ff37ae178
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2024-01-13 18:32:04 -0500

    support anchor,positive,and negative augmentation

--- responder/augmentor/aug.py
+++ responder/augmentor/aug.py
@@ -12,23 +12,24 @@ import numpy as np
 class MixupNomralAugmentor:
     """Mixup from normal samples with selected genes"""
 
-    def __init__(self, df_tpm_normal, genes2mixup=[], beta = 0.9, n_views=1):
+    def __init__(self, df_tpm_normal, genes2mixup=[], 
+                 mix_beta = 0.9, n_views=1):
         '''
         Selective mixup
         '''
+    
         if len(genes2mixup) == 0:
             genes2mixup = df_tpm_normal.columns
             
         X = torch.tensor(df_tpm_normal.values,dtype=torch.float32).clone().detach()
-        #X = normalize(torch.log2(X + 1.), p=2.0, dim = 0)
         
         self.genes2mixup = genes2mixup
         self.select_idx = df_tpm_normal.columns.get_indexer(genes2mixup)
         self.X_mix = X[:, self.select_idx]
 
         self.n = len(df_tpm_normal)
-        self.beta = beta
-        self.p =  Beta(torch.FloatTensor([beta]), torch.FloatTensor([beta]))
+        self.beta = mix_beta
+        self.p =  Beta(torch.FloatTensor([mix_beta]), torch.FloatTensor([mix_beta]))
         self.n_views = n_views
 
     ## remember to do a select mixup
@@ -56,62 +57,101 @@ class MixupNomralAugmentor:
 class RandomMaskAugmentor:
     """Random Mask Augmentation for Gene Expression Vectors"""
 
-    def __init__(self, mask_probability = 0.01, mask_self=True, mask_self_probability = 0.5, n_views=1):
+    def __init__(self, 
+                 mask_p_prob = 0.5,
+                 mask_a_prob = None, 
+                 mask_n_prob = None,
+                 n_views=1):
         '''
         Initialize the random mask augmentor with the given data and parameters.
         '''
-        self.mask_probability = mask_probability
-        self.mask_self = mask_self
-        self.mask_self_probability = mask_self_probability
+        if mask_a_prob is None:
+            mask_a_prob = mask_p_prob
+
+        if mask_n_prob is None:
+            mask_n_prob = 0.0
+            
+        self.mask_p_prob = mask_p_prob
+        self.mask_a_prob = mask_a_prob
+        self.mask_n_prob = mask_n_prob
         self.n_views = n_views
 
         
-    def _transform(self, x):
+    def _transform(self, x, probability):
         
-        if self.mask_self:
-            # to mask the x itself
-            x_self = x.numpy()
-            mask = np.random.rand(*x_self.shape) < self.mask_self_probability
-            x_self[mask] = 0
-
         x_np = x.clone().numpy()
         random_values = np.random.rand(*x_np.shape)
-        mask = random_values < self.mask_probability
+        mask = random_values < probability
         x_np[mask] = 0
         x_new = torch.from_numpy(x_np).to(x.device)
         return x_new
-        
 
-    def augment(self, x):
-        return [self._transform(x) for _ in range(self.n_views)]
+    def _augment(self, x, mask_prob):
+        return [self._transform(x, mask_prob) for _ in range(self.n_views)]
 
-    
+    def augment_p(self, p):
+        return  self._augment(p, self.mask_p_prob)
+
+    def augment_a(self, a):
+        return  self._augment(a, self.mask_a_prob)
+
+    def augment_n(self, n):
+        return  self._augment(n, self.mask_n_prob)
+
+
     def __repr__(self):
-        return self.__class__.__name__ + '(mask_probability={}, n_views={})'.format(self.mask_probability, 
+        return self.__class__.__name__ + '(mask_probability=(a:{},p:{},n:{}), n_views={})'.format(
+                                                                                    self.mask_a_prob,
+                                                                                    self.mask_p_prob,
+                                                                                    self.mask_n_prob,
                                                                                     self.n_views)
 
 
 class FeatureJitterAugmentor:
     """Feature Jittering for Gene Expression Vectors"""
 
-    def __init__(self, jitter_std = 0.01, n_views=1):
+    def __init__(self, 
+                 jitter_p_std = 0.2,
+                 jitter_a_std = None, 
+                 jitter_n_std = None,
+                 n_views=1):
         '''
         Initialize the feature jitter augmentor with the given data and parameters.
         '''
-        self.jitter_std = jitter_std
+        if jitter_a_std is None:
+            jitter_a_std = jitter_p_std
+        if jitter_n_std is None:
+            jitter_n_std = 0.0
+            
         self.n_views = n_views
+        self.jitter_p_std = jitter_p_std
+        self.jitter_a_std = jitter_a_std
+        self.jitter_n_std = jitter_n_std
 
-    def _transform(self, x):
+    
+    def _transform(self, x, jitter_std):
         # Generate jittering noise
-        jitter = torch.normal(mean=0, std=self.jitter_std, size=x.size())
-
+        jitter = torch.normal(mean=0, std=jitter_std, size=x.size())
         # Apply jittering to the vector
         x_jittered = x.clone() + jitter
-
         return x_jittered.to(x.device)
 
-    def augment(self, x):
-        return [self._transform(x) for _ in range(self.n_views)]
+    def _augment(self, x, jitter_std):
+        return [self._transform(x, jitter_std) for _ in range(self.n_views)]
 
+    def augment_p(self, p):
+        return  self._augment(p, self.jitter_p_std)
+
+    def augment_a(self, a):
+        return  self._augment(a, self.jitter_a_std)
+
+    def augment_n(self, n):
+        return  self._augment(n, self.jitter_n_std)
+
+    
     def __repr__(self):
-        return self.__class__.__name__ + '(jitter_std={}, n_views={})'.format(self.jitter_std, self.n_views)
+        return self.__class__.__name__ + '(jitter_std=(a:{},p:{},n:{}), n_views={})'.format(
+                                                                                    self.jitter_a_std,
+                                                                                    self.jitter_p_std,
+                                                                                    self.jitter_n_std,
+                                                                                    self.n_views)
