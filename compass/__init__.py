f21a6a262d7ca571f5ec2b02c49304e19e8daed3
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2024-07-16 14:59:24 -0400

    rename conceptor to compass

---
+++ compass/__init__.py
@@ -0,0 +1,4 @@
+from .main import PreTrainer, FineTuner, loadconceptor
+from .main import Adapter
+
+
---
+++ compass/utils/__init__.py
@@ -0,0 +1,3 @@
+from .scorer import score, score2
+from .plotter import plot_embed_with_label, plot_performance 
+from .sankey import plot_sankey_diagram
