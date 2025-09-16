59f0b9b775a8b910c49dd5e0f67e6762b544a731
shenwanxiang <shenwanxiang@tsinghua.org.cn>
2025-09-16 22:30:24 +0800

    add CRMap code

--- compass/utils/__init__.py
+++ compass/utils/__init__.py
@@ -2,3 +2,4 @@ from .scorer import score, score2, score3
 from .plotter import plot_embed_with_label, plot_performance
 from .sankey import plot_sankey_diagram, get_projector_weights
 from .loader import loadcompass
+from .crmap import prepare_crmap_data, personal_crmap_data, draw_personal_crmap
